5e8932327ab94ac52c60a5dc6f15ff9e52434e9a
carlos alberto <carlosalberto.sl12@hotmail.com>
2023-10-22 17:01:04 -0300

    first commit

---
+++ br/onload.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+    
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+}
---
+++ br/typing-animation.js
@@ -0,0 +1,37 @@
+let typingSpeed = 50;
+const max = 50;
+const min = 25;
+let currentLine = 0;
+
+const typeText = () => {
+    const zones = document.querySelectorAll('.cmd');
+    zones.forEach(zone => {
+        const lines = zone.querySelectorAll('.typing-animation');
+        if (lines && lines.length > 0) {
+            const text = lines[0].getAttribute('data-text');
+            lines[0].classList.add('currently-typing');
+            typeLetter(lines[0], text, 0, lines);
+        }
+    });
+};
+
+const typeLetter = (line, text, position, allLines) => {
+    if (position < text.length) {
+        line.textContent += text.charAt(position);
+        typingSpeed = Math.floor(Math.random() * (max - min + 1)) + min;
+        setTimeout(() => typeLetter(line, text, position + 1, allLines), typingSpeed);
+    } else {
+        line.classList.remove('currently-typing');
+        if (allLines.length > 1) {
+            allLines[1].classList.add('currently-typing');
+            const nextText = allLines[1].getAttribute('data-text');
+            typeLetter(allLines[1], nextText, 0, Array.from(allLines).slice(1));
+        } else {
+            line.classList.add('last-typed');
+        }
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  typeText();
+});
